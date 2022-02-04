5c192eac551b14ad06e2a8905bc9baac1b6db5b0
Kat Simons <Kat.Simons@vu.com>
2022-02-04 13:09:15 -0600

    try

--- gradeCalculator.py
+++ gradeCalculator.py
@@ -3,21 +3,24 @@ def calculateGrade():
     print("Calculating Grade")
     # This first line is provided for you
 
-    score = float(input("Enter score:"))
+    try:
 
-    if score > 1:
-        print ("Bad score") 
-    elif score >= 0.9:
-        print ("A")
-    elif score >= 0.8:
-        print ("B")
-    elif score >= 0.7:
-        print ("C")
-    elif score >= 0.6:
-        print ("D")
-    elif score < 0.6:
-        print ("F")
+        score = float(input("Enter score:"))
 
+        if score > 1:
+            print ("Bad score") 
+        elif score >= 0.9:
+            print ("A")
+        elif score >= 0.8:
+            print ("B")
+        elif score >= 0.7:
+            print ("C")
+        elif score >= 0.6:
+            print ("D")
+        elif score < 0.6:
+            print ("F")
+    except: 
+        print ("Bad Score")
 
     # end assignment
 
